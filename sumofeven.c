ef749a495822e0a5229661ac7cd178e9df60e7a7
ChristinRaju <2006christinraju@gmail.com>
2024-10-20 21:41:08 +0530

    Add files via upload

---
+++ sumofeven.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int N, sum = 0;
+
+    // Take input for N
+    printf("Enter a positive integer N: ");
+    scanf("%d", &N);
+
+    // Calculate the sum of even numbers
+    for (int i = 2; i <= N; i += 2) {
+        sum += i;
+    }
+
+    // Display the result
+    printf("The sum of all even numbers from 1 to %d is: %d\n", N, sum);
+
+    return 0;
+}
