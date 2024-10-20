68d7136e96182ef916076f0ce5b97a890c8f3079
ChristinRaju <2006christinraju@gmail.com>
2024-10-20 20:58:38 +0530

    Add files via upload

---
+++ primeno.c
@@ -0,0 +1,27 @@
+#include<Stdio.h>
+int main()
+{
+    int num, i;
+
+    printf("enter any number\n");
+    scanf("%d", &num);
+
+    if(num < 2)
+    {
+        printf("%d is not a prime number.\n", num);
+        return 0;
+    }
+
+    for(i = 2; i < num; i++)
+    {
+        if(num % i == 0)
+        {
+            printf("%d is not a prime number.\n", num);
+            return 0;
+        }
+    }
+
+    printf("%d is a prime number.\n", num);
+    return 0;
+
+}    
