d58eb97db94af7d10e695839fe181baa2d0905b2
ChristinRaju <2006christinraju@gmail.com>
2024-10-20 21:50:04 +0530

    Add files via upload

---
+++ pos_neg_zero.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+ {
+    float num;
+
+    printf("Enter a number: ");
+    scanf("%f", &num);
+
+    // Check if the number is positive, negative, or zero
+    if (num > 0) 
+    {
+        printf("The number %f is positive\n", num);
+    } 
+        else if (num < 0)
+        {
+            printf("The number %f is negative\n", num);
+        } 
+            else
+            {
+                printf("The number is zero\n");
+            }
+
+    return 0;
+}
