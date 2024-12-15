da7c8d8c92ec251f904c0a6f7d753e65c82bb71f
ChristinRaju <2006christinraju@gmail.com>
2024-12-15 18:24:45 +0530

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,20 @@
+#include<Stdio.h>
+int main(){
+
+    int a = 10, *p, **q;
+    p = &a;
+    q = &p;
+
+    printf("%d\n", a); //10
+    printf("%d\n", &a); // address of a
+    printf("%d\n", p); //address of a
+    printf("%d\n", *p); //10
+    printf("%d\n", &p); //address of p
+    
+    printf("%d\n", q); //address of p
+    printf("%d\n", *q); //address of a
+    printf("%d\n", **q); //10
+    printf("%d\n", &q); //address of q
+
+    return 0;
+}
